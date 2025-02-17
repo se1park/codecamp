b159dbad70bbcf1bc8342d683fccd28f73c29932
sewon <swon7150@gmail.com>
2025-02-17 23:52:09 +0900

    first

---
+++ pages/section_02/02_02_signup_state/index.js
@@ -0,0 +1,15 @@
+export default function SignupStatePage() {
+
+    function onClickSignup() {
+
+    }
+
+    return(
+        <div>
+            이메일: <input type="text" />
+            비밀번호: <input type="password" />
+            <button onClick={onClickSignup}>회원가입</button>
+        </div>
+    )
+
+}
